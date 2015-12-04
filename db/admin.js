f37f95234acf47729bffcf81f2b6f686af5c5a33
samelie <sam@stinkdigital.com>
2015-12-04 18:55:54 +0000

    removed config

--- db/admin.js
+++ db/admin.js
@@ -1,9 +1,5 @@
 var configForAdmin = {
-	"username": "rad",
-    "password": "kingdeth",
-    "database": "radmix",
-    "host": "radfree.cbxauq0ss8tv.eu-west-1.rds.amazonaws.com",
-    "dialect": "postgres"
+	
 };
 
 
